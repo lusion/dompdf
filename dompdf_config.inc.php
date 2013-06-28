b29eef49c65fd4cc02f50f91513935021aeb8b43
Josh Yudaken <j@yud.co.za>
2013-06-28 14:08:33 +0200

    Minor fixes for SnapBill

--- dompdf_config.inc.php
+++ dompdf_config.inc.php
@@ -332,7 +332,9 @@ require_once(DOMPDF_LIB_DIR . "/html5lib/Parser.php");
  */
 if (DOMPDF_ENABLE_AUTOLOAD) {
   require_once(DOMPDF_INC_DIR . "/autoload.inc.php");
-  require_once(DOMPDF_LIB_DIR . "/php-font-lib/classes/font.cls.php");
+
+  // We do not require pdf-font-lib since we use no custom fonts
+  // require_once(DOMPDF_LIB_DIR . "/php-font-lib/classes/font.cls.php");
 }
 
 /**
